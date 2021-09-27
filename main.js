a9a3de0a5a0402c9573290594b8e0b3bc7525e82
jdkrei65 <79920106+jdkrei65@users.noreply.github.com>
2021-09-27 16:28:05 -0400

    Created a moveable player

--- main.js
+++ main.js
@@ -1,8 +1,33 @@
+// Player position
+let position = {
+  x: 150,
+  y: 100
+}
+
 function setup() {
-    createCanvas(400, 400);
+  createCanvas(600, 400);
+  strokeWeight(0);
+  fill(0);
+}
+
+// Player motion with the arrow keys
+function move() {
+  if (keyIsDown(RIGHT_ARROW)) {
+    position.x += 1;
+  } else if (keyIsDown(LEFT_ARROW)) {
+    position.x -= 1;
+  }
+  if (keyIsDown(UP_ARROW)) {
+    position.y -= 1;
+  } else if (keyIsDown(DOWN_ARROW)) {
+    position.y += 1;
+  }
 }
 
 function draw() {
-    background(220);
-    ellipse(50,50,80,80);
+  move();
+  background(255);
+  circle(position.x, position.y, 30);
+  textSize(32);
+  text('Use Arrow Keys To Move', 10, 30);
 }
