f72e3e0cf46212bdb6ede493b86880bed3cc954c
jkrei0 <jkrei021@gmail.com>
2022-02-14 20:25:49 -0500

    Added player, exported game

---
+++ resources/Player.gd
@@ -0,0 +1,31 @@
+extends KinematicBody2D
+
+
+export var speed = 150
+export var gravity = 25
+export var jump_power = 500
+var velocity = Vector2(0, 0)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _physics_process(delta):
+	if Input.is_action_pressed("move_right"):
+		velocity.x = speed
+	if Input.is_action_pressed("move_left"):
+		velocity.x = -speed
+		
+	velocity.y += gravity
+	
+	if Input.is_action_pressed("jump") and is_on_floor():
+		velocity.y = -jump_power
+	
+	velocity = move_and_slide(velocity, Vector2.UP)
+	
+	velocity.x = lerp(velocity.x, 0, 0.1)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
